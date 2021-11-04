5bcc4a3e08a25333c9e5331d6581c1bd1e01cc10
SandraCastroPearson <38700157+SandraCastroPearson@users.noreply.github.com>
2021-11-03 19:07:59 -0500

    return to old cindex

--- R/2SCRcindex.R
+++ R/2SCRcindex.R
@@ -20,74 +20,55 @@
 comprsk_c <- function(mymat, rlabels, maxt){
   cens <- ifelse(mymat$event==0, 1, 0)
   GKM <- summary(survfit(Surv(mymat$time, cens) ~ 1, type='kaplan-meier'))
-  #mymat <- mymat[mymat$time<maxt,]
+  mymat <- mymat[mymat$time<=maxt,]
 
   a=data.table(mymat); a[,merge:=a$time]
   b=data.table(time = GKM$time, w = GKM$surv); b[,merge:=b$time]
   setkeyv(a,c('merge')); setkeyv(b,c('merge'))
   mymat=b[a,roll=+1]
-  indsna <- which(is.na(mymat$w)&mymat$i.time<maxt)
+  indsna <- which(is.na(mymat$w))
+  mymat$time[indsna] = 0;
   mymat$w[indsna] = 1;
   mymat <- mymat[,-c(1,3)]
-  indsna <- which(is.na(mymat$w)&mymat$i.time>maxt)
-  mymat$w[indsna] = 0;
   n <- nrow(mymat)
+  pcomb <- n*(n-1)/2
   `%!in%` <- Negate(`%in%`)
-  colnames(mymat)[3] = "time"
-  inds <- which(mymat$time %in% GKM$time)
-  mymat$w2 <- mymat$w
-  if (1 %in% inds){
-    mymat$w2[1] = 1
-    inds <- inds[-which(inds==1)]
-  }
-  for (ic in inds){
-    mymat$w2[ic] <- mymat$w2[ic-1]
-  }
 
-  c_h = c_p = c_u = c_w1 = c_w2 = c_w3 = c_u2 = NULL;
-  denom = denom2 = denom3 = denom4 = denom5 = denom6 = 0
-  num = num2 = num3 = num4 = num5 = num6 = Q = cij = 0
+  c=NULL; dnomcount = numcount = 0
   for (e in 1:max(mymat$event)){
-    for (i in 1:n){
-      for (j in 1:n){
-        if (i!=j){
-        #     if (mymat$event[i]==e & mymat$event[j]==e){
-        #       Q = Q + 1
-        #       cij = cij + ifelse(mymat$group[i]<mymat$group[j] & mymat$time[i]<mymat$time[j], 1,
-        #                          ifelse(mymat$group[i]>mymat$group[j] & mymat$time[i]>mymat$time[j],1,0))
-        #     }
-        if (mymat$time[i]<maxt){
-          temp1 <- ifelse((mymat$group[i]<mymat$group[j]& mymat$event[i]==e)|
-                            (mymat$group[i]>=mymat$group[j] & mymat$event[i] %!in% c(0,e)), 1, 0)
-                  #
-          temp2 <- ifelse(mymat$time[i]<mymat$time[j] & ((mymat$group[i]<mymat$group[j]& mymat$event[i]==e)|
-                            (mymat$group[i]>=mymat$group[j] & mymat$event[i] %!in% c(0,e))), 1, 0)
-          denom = denom + temp1; num = num + temp2;
-          ####
-          if (mymat$w[i]!=0 & mymat$w[j]!=0 & mymat$w2[i]!=0 & mymat$w2[j]!=0){
-            denom2 = denom2 + temp1/(mymat$w2[i]^2); num2 = num2 + temp2/(mymat$w2[i]^2)
-            #####
-            denom3 = denom3 + temp1/(mymat$w[i]*mymat$w[j]); num3 = num3 + temp2/(mymat$w[i]*mymat$w[j])
-            #####
-            denom4 = denom4 + temp1/(mymat$w[i]*mymat$w2[j]); num4 = num4 + temp2/(mymat$w[i]*mymat$w2[j])
-            #####
-            denom5 = denom5 + temp1/(mymat$w2[i]*mymat$w[j]); num5 = num5 + temp2/(mymat$w2[i]*mymat$w[j])
-            #####
-            denom6 = denom6 + temp1/(mymat$w[i]^2); num6 = num6 + temp2/(mymat$w[i]^2)
-            #####
-            #denom7 = denom7 + temp1/(mymat$w2[i]*mymat$w[j]); num5 = num5 + temp2/(mymat$w2[i]*mymat$w[j])
-            }
-          }
+    for (i in 1:n1){
+      for (j in 1:n0){
+        if (i==j){dnomcount=dnomcount; numcount=numcount
+        } else {
+          temp <- ifelse(mymat$event[i]==e & mymat$event[j]==e & mymat$group[i]==1 & mymat$group[j]==0, 1, 0)
+          dnomcount = dnomcount + temp
+          temp2 <- ifelse(mymat$i.time[j] <= mymat$i.time[i] & mymat$event[i]==e & mymat$event[j]==e &
+                            mymat$group[i]==1 & mymat$group[j]==0, 1,0)
+          numcount = numcount + temp2
         }
       }
     }
-     c_p[e] <- num/denom; c_u[e] <- num2/denom2; #c_h[e] <- cij/Q;
-    c_w1[e] <- num3/denom3; c_w2[e] <- num4/denom4; c_w3[e] <- num5/denom5; c_u2[e] = num6/denom6
+    c[e] <- numcount/dnomcount
   }
-  #names(c_h) =
-  names(c_p) = names(c_u) = names(c_u2) = names(c_w1) = names(c_w2) = names(c_w3) = rlabels
+  names(c) <- rlabels
 
-  return(list( c_p = c_p, c_u=c_u, c_u2 = c_u2, c_w1=c_w1, c_w2=c_w2, c_w3=c_w3)) #c_h=c_h,
+  c2=c(); dnomcount = numcount = 0.000000000000000001
+  for (e in 1:max(mymat$event)){
+    for (i in 1:n1){
+      for (j in 1:n0){
+        if (i!=j){
+          temp <- ifelse(mymat$event[i]==e & mymat$event[j]==e & mymat$group[i]==1 & mymat$group[j]==0, 1, 0)
+          dnomcount = dnomcount + temp/(mymat$w[i]*mymat$w[j])
+          temp2 <- ifelse(mymat$i.time[j] <= mymat$i.time[i] & mymat$event[i]==e & mymat$event[j]==e &
+                            mymat$group[i]==1 & mymat$group[j]==0, 1,0)
+          numcount = numcount + temp2/(mymat$w[i]*mymat$w[j])
+        }
+      }
+    }
+    c2[e] <- (numcount-0.000000000000000001)/(dnomcount-0.000000000000000001)
+  }
+  names(c2) <- rlabels
+
+  return(list(c=c, c_adj = c2))
 }
-
 
